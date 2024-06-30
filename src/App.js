89d210bbc127bd9bc2cf32f451353a5603345776
unknown <Louis Pierre Andre>
2024-06-30 12:58:56 -0400

    Configure Netlify CMS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and Welcom to website 300, okey....,?.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Welcom to Web3000</h1>>
     </div>
   );
 }
